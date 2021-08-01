d9143dce8d50497796d51a6e504427167b5e2366
StewStrong <stew.strong.420@gmail.com>
2021-08-01 15:10:50 -0600

    Fixed linter errors

--- src/main/kotlin/org/valkyrienskies/physics_api_krunch/KrunchRigidBodyTransform.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api_krunch/KrunchRigidBodyTransform.kt
@@ -4,7 +4,8 @@ import org.joml.Quaterniond
 import org.joml.Vector3d
 import org.valkyrienskies.physics_api.RigidBodyTransform
 
-internal data class KrunchRigidBodyTransform(override val position: Vector3d, override val rotation: Quaterniond) : RigidBodyTransform {
+internal data class KrunchRigidBodyTransform(override val position: Vector3d, override val rotation: Quaterniond) :
+    RigidBodyTransform {
     companion object {
         fun createEmptyKrunchRigidBodyTransform() = KrunchRigidBodyTransform(Vector3d(), Quaterniond())
     }
