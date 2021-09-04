0827ae2772ff085eea372140e9ef910d0dea52bc
StewStrong <stew.strong.420@gmail.com>
2021-09-04 03:01:03 -0600

    Updated Krunch

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     api("org.valkyrienskies:physics_api:1.0.0+e6f7e5f1c5")
 
     // Krunch
-    implementation("org.valkyrienskies:krunch:1.0.0+a7f6e3ce56")
+    implementation("org.valkyrienskies:krunch:1.0.0+f2da0a852b")
 
     // JOML for Math
     api("org.joml", "joml", "1.10.0")
