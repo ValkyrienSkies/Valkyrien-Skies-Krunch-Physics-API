af9f5b6667e0e91720856c0fb5fd0e2f88edc80c
StewStrong <stew.strong.420@gmail.com>
2021-09-04 02:51:29 -0600

    Updated Krunch to use speculative contacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     api("org.valkyrienskies:physics_api:1.0.0+e6f7e5f1c5")
 
     // Krunch
-    implementation("org.valkyrienskies:krunch:1.0.0+9f4ef41d06")
+    implementation("org.valkyrienskies:krunch:1.0.0+a7f6e3ce56")
 
     // JOML for Math
     api("org.joml", "joml", "1.10.0")
