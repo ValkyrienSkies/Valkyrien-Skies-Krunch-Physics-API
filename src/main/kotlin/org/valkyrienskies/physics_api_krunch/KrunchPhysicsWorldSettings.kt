99834d5c40ae52c27b5d7fc2cb6aa4fdc45eeecf
StewStrong <stew.strong.420@gmail.com>
2021-09-04 22:44:13 -0600

    Fixed style error

--- src/main/kotlin/org/valkyrienskies/physics_api_krunch/KrunchPhysicsWorldSettings.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api_krunch/KrunchPhysicsWorldSettings.kt
@@ -2,4 +2,4 @@ package org.valkyrienskies.physics_api_krunch
 
 import org.valkyrienskies.krunch.KrunchPhysicsWorldSettings
 
-class KrunchPhysicsWorldSettings: KrunchPhysicsWorldSettings()
+class KrunchPhysicsWorldSettings : KrunchPhysicsWorldSettings()
